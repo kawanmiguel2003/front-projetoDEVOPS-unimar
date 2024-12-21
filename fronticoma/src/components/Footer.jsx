319febf913ba2ff513d26417750ade2915dbbe28
victoricoma-soulcode <victor.icoma@soulcode.com>
2024-12-21 09:05:00 -0300

    Montagem das configurações de ambiente

--- fronticoma/src/components/Footer.jsx
+++ fronticoma/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <h3>Escreva sobre o que você tem interesse!</h3>
-      <p>Fatec Dev Team &copy; 2024</p>
+      <p>Unimar Dev Team &copy; 2024</p>
       <img src={logo} alt="Logo do Developer Mini Blog" />
     </footer>
   );
