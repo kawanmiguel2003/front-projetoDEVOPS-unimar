6b04324af05d6fb2ded0ceed84603544aa12d84c
kawanmiguel2003 <miguel.kawan2003@gmail.com>
2025-01-23 20:48:28 -0300

    EntregaViaAzureBoards

--- fronticoma/src/App.jsx
+++ fronticoma/src/App.jsx
@@ -50,6 +50,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
+              
               <Route
                 path="/posts/create"
                 element={user ? <CreatePost /> : <Navigate to="/login" />}
